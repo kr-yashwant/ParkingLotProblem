f22d265e30ea75783945da9a4f228fee81c9080d
Kumar Yashwant <kr.yashwant@outlook.com>
2017-12-13 16:33:20 +0530

    Adding Initial Conditions, Test and Code

---
+++ src/main/java/com/entities/ParkingLot.java
@@ -0,0 +1,14 @@
+package com.entities;
+
+public class ParkingLot {
+    /**
+     * Assumes the precondition that the Parking Lots at this time can
+     * accommodate infinite number of cars, based on which condition
+     * the method always returns true
+     * @param car
+     * @return
+     */
+    public boolean accommodate(Car car) {
+        return true;
+    }
+}
