594e0db3d91462c2f5e538d6c28fe97623f1f2f2
Kumar Yashwant <kr.yashwant@outlook.com>
2017-12-14 11:39:48 +0530

    Adding Initial Conditions, Test and Code

--- src/main/java/com/entities/ParkingLot.java
+++ src/main/java/com/entities/ParkingLot.java
@@ -11,12 +11,16 @@ public class ParkingLot {
     }
 
     public boolean park(Car car) {
+        if(this.has(car.getId())) {
+            throw new IllegalArgumentException("A car is already parked with this registration number");
+        }
         this.parkedCarNums.add(car.getId());
         return true;
     }
 
     public boolean unPark(int carId) {
         if(this.has(carId)) {
+            parkedCarNums.remove(carId);
             return true;
         }
         throw new IllegalArgumentException("This car is not parked in this lot");
--- src/test/java/ParkingLotTest.java
+++ src/test/java/ParkingLotTest.java
@@ -3,30 +3,44 @@ import com.entities.ParkingLot;
 import static org.junit.Assert.*;
 import org.junit.Test;
 
-import java.util.Random;
 
 public class ParkingLotTest {
 
-    private Random random = new Random();
-    private ParkingLot parkingLot = new ParkingLot();
-
     @Test
     public void validateIfCarCanBeParked() {
-        int carNumber = random.nextInt();
+        ParkingLot parkingLot = new ParkingLot();
+        int carNumber = 999;
         Car car = new Car(carNumber);
         assertTrue(parkingLot.park(car));
     }
 
     @Test
     public void validateIfTheCarCanBeUnparked() {
-        Car car = new Car(random.nextInt());
+        ParkingLot parkingLot = new ParkingLot();
+        Car car = new Car(888);
         parkingLot.park(car);
         assertTrue(parkingLot.unPark(car.getId()));
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void validateTheExceptionIsThrownIfRequestedCarIsNotThere() {
-        parkingLot.unPark(random.nextInt());
+        ParkingLot parkingLot = new ParkingLot();
+        parkingLot.unPark(1000);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void validateExceptionIsThrownUponMultipleUnparkingAttempts() {
+        ParkingLot parkingLot = new ParkingLot();
+        parkingLot.park(new Car(2000));
+        parkingLot.unPark(2000);
+        parkingLot.unPark(2000);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void validateIfAParkedCarCanBeParkedWithoutUnparking() {
+        ParkingLot parkingLot = new ParkingLot();
+        parkingLot.park(new Car(2000));
+        parkingLot.park(new Car(2000));
     }
 
 }
